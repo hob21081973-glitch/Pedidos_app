cce2dfe24451bfd020f87818fbec06b420a850b5
hob21081973-glitch <hob21081973@gmail.com>
2026-08-24 17:05:12 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sistema de Pedidos',
+      title: 'Sistema Pedidos',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.green,
